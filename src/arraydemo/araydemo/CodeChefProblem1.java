58cc6f43b663465433428b9e8af7c94546d89b90
Vinay Yadav <vinayyadav91190@gmail.com>
2022-11-09 23:41:21 +0530

    update the code

--- src/arraydemo/araydemo/CodeChefProblem1.java
+++ src/arraydemo/araydemo/CodeChefProblem1.java
@@ -10,25 +10,30 @@ public class CodeChefProblem1 {
 		while (t > 0) {
 			int n = sc.nextInt();
 			String s = sc.next();
-			int l = (n / 2) - 1;
-			int l1 = l;
-			int r = n - 1;
-			boolean response = true;
-			while (l >= 0 && r > l1) {
-				System.out.print(l + " " + r + " ");
-				if (s.charAt(l) != s.charAt(r)) {
-					response = false;
-					System.out.println("No");
-					break;
-				}
-				l--;
-				r--;
-			}
-			if (response == true)
+			String s2 = s.substring(n / 2, n);
+			if (s.substring(0, n / 2).equals(s2))
 				System.out.println("Yes");
+			else
+				System.out.println("No");
+			// int l = (n / 2) - 1;
+			// int l1 = l;
+			// int r = n - 1;
+			// boolean response = true;
+			// while (l >= 0 && r > l1) {
+			// System.out.print(l + " " + r + " ");
+			// if (s.charAt(l) != s.charAt(r)) {
+			// response = false;
+			// System.out.println("No");
+			// break;
+			// }
+			// l--;
+			// r--;
+			// }
+			// if (response == true)
+			// System.out.println("Yes");
 
-			t--;
-			sc.close();
+			// t--;
 		}
+		sc.close();
 	}
 }
